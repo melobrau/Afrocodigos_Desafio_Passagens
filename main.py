abcd4aef035b0b7b7b7434a4a4f6dc89dae541a7
Mateus Melo <melo.artecontato@gmail.com>
2024-03-29 00:13:55 -0300

    COMMIT FINAL

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from passagens import PassagensAereasManager
+from passagens import PassagensAereasManager, Passagem
 
 from menu import mostra_menu
 
+passagens_aereas_manager = PassagensAereasManager()
 
 while True:
-
-    passagens_aereas_manager = PassagensAereasManager()
 
     #MOSTRAR O MENU
     mostra_menu()
@@ -14,13 +13,18 @@ while True:
     #RECEBER ENTRADA DO USUARIO
     if entrada_do_usuario == 1:
         print("Voce escolheu comprar passagens")
-        print("Digite o destino da sua viagem:")
-        print("digite agora o local de partida:")
-        print("Digite o valor da sua passagem:")
+
+        destino = input("Digite o destino da sua viagem:")
+        origem = input("Digite agora o local de partida:")
+        valor = input("Digite o valor da sua passagem:")
+
+        passagem = Passagem(origem, destino, valor)
+
+        passagens_aereas_manager.adicionar_passagem(passagem)
 
     elif entrada_do_usuario == 2:
-        print("Segue abaixo uma lista das suas passagens:")
-        print(passagens_aereas_manager.passagens_compradas)
+            print("Segue abaixo uma lista das suas passagens:")
+            print(passagens_aereas_manager.passagens_compradas)
 
     #EM CASO DE ENTRADA INVALIDA
 #   if  entrada_do_usuario != [1,2,3]:
@@ -28,4 +32,4 @@ while True:
     #SAIDA DO PROGRAMA
     elif entrada_do_usuario == 3:
         print("Encerrando o programa. Valeu! Volte sempre!")
-    break
+        break
--- passagens.py
+++ passagens.py
@@ -1,5 +1,17 @@
 class PassagensAereasManager():
-        passagens_compradas = ["FAKE"]
 
-        def adicionar_passagem(self,passagem):
+        def __init__(self):
+            self.passagens_compradas = []
+        
+        def adicionar_passagem(self, passagem):
             self.passagens_compradas.append(passagem)
+
+class Passagem:
+    def __init__(self, origem, destino, valor):
+         self.origem = origem
+         self.destino = destino
+         self.valor = valor 
+
+         
+    def __repr__(self):        
+         return f"Passagem => De'{self.origem}', PARA'{self.destino}', PAGANDO={self.valor})"
